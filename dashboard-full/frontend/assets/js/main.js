9fe14be73dc55064fb24ee8d4a06bd564499a948
imamsyahjr-sketch <imamsyahjr@gmail.com>
2026-02-10 12:08:56 +0700

    Delete dashboard-full/frontend directory

--- dashboard-full/frontend/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const dateElement = document.getElementById("currentDate");
-
-const today = new Date();
-dateElement.textContent = today.toLocaleDateString("id-ID", {
-  weekday: "long",
-  day: "numeric",
-  month: "long",
-  year: "numeric"
-});
